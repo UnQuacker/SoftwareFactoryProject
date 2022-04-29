aa9fedb8ca994a393819cf9342ca520faa7dd100
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-04-29 16:41:27 +0600

    Initial login/register

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 app_name = 'main'
 
 urlpatterns = [
     path('kettik', views.kettik, name='kettik'),
     path('main', views.main_page, name='main'),
-    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     path('search', views.search, name='search'),
+    path('register', views.postsignUp, name='register')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
+import traceback
+
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
+import pyrebase
+
+firebaseConfig = {
+    'apiKey': "AIzaSyA7AKnjaTU0PdvPVC0IrlxyDTktHwSG4AA",
+    'authDomain': "software-factory-ce597.firebaseapp.com",
+    'projectId': "software-factory-ce597",
+    'storageBucket': "software-factory-ce597.appspot.com",
+    'messagingSenderId': "160550003152",
+    'appId': "1:160550003152:web:1c02e7bfdf36959fa519d5",
+    'measurementId': "G-EVZTV7SBHL",
+    'databaseURL': ""
+
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+authe = firebase.auth()
+database = firebase.database()
 
 
 def kettik(request):
@@ -16,3 +34,52 @@ def register(request):
 
 def search(request):
     return render(request, 'main/search.html')
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+def postLogin(request):
+    if request.method == 'POST':
+        email = request.POST.get('login_email')
+        password = request.POST.get('login_password')
+        try:
+            user = authe.sign_in_with_email_and_password(email, password)
+            return
+        except:
+            message = "Invalid credentials!"
+            return render(request, 'main/registerligin.html', {'loginMessage': message})
+    return render(request, 'main/index.html')
+
+
+def postsignUp(request):
+    if request.method == 'POST':
+        emailReg = request.POST.get('register_email', False)
+        passsReg = request.POST.get('register_password1')
+        nameReg = request.POST.get('register_name')
+
+        emailLog = request.POST.get('login_email', False)
+        passwordLog = request.POST.get('login_password')
+        if emailReg:
+            try:
+                # creating a user with the given email and password
+                user = authe.create_user_with_email_and_password(emailReg, passsReg)
+                uid = user['localId']
+                idtoken = request.session['uid']
+                # print(uid)
+                return render(request, "main/homepage.html")
+            except Exception as e:
+                message = "invalid credentials!"
+                return render(request, 'main/registerligin.html', {'message': message})
+                pass
+        elif emailLog:
+            try:
+                user = authe.sign_in_with_email_and_password(emailLog, passwordLog)
+                return render(request, "main/homepage.html")
+            except Exception as e:
+                message = "invalid credentials!"
+                return render(request, 'main/registerligin.html', {'loginMessage': message})
+    else:
+        return render(request, 'main/registerligin.html')
+    # return render(request, "main/homepage.html")
